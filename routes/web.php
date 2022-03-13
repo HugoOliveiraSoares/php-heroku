a2745939b2d6b79309e9c9b907b4bbaad17e0d31
Hugo Oliveira Soares <hugoliveira.soares@gmail.com>
2022-03-13 08:57:12 -0300

    Rotas no laravel

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
+});
+
+Route::get('/series', function () {
+
+  $serires = [
+    'Grey\'s Anatomy',
+    'Lost',
+    'Agents of SHIELD'
+  ];
+
+  $html = "<ul>";
+  foreach ($serires as $serie) {
+
+    $html .= "<li>$serie</li>";
+  }
+  $html .= "<ul>";
+
+  return $html;
 });
